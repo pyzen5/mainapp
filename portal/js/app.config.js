086fb078acfaf46a7b9d467f1b2646e453005693
pyzen5 <pyzen55@gmail.com>
2016-12-14 23:09:03 +0530

    partial ga

--- portal/js/app.config.js
+++ portal/js/app.config.js
@@ -1,5 +1,10 @@
-angular.module('mainMod').config(function($urlRouterProvider,$stateProvider){
-    //$urlRouterProvider.otherwise('/');
+angular.module('mainMod')
+.config(config)
+.run(run);
+    config.$inject = ['$locationProvider', '$stateProvider', '$urlRouterProvider'];
+    function config($locationProvider,$stateProvider,$urlRouterProvider){
+ 
+        $urlRouterProvider.otherwise("/");
     $stateProvider.state('home',{
         url:'/home',
         templateUrl:'partials/home.html'
@@ -8,4 +13,15 @@ angular.module('mainMod').config(function($urlRouterProvider,$stateProvider){
         url:'/about',
         templateUrl:'partials/about.html'
     })
-})
+    }
+
+    run.$inject = ['$rootScope', '$location', '$window'];
+    function run($rootScope, $location, $window) {
+        // initialise google analytics
+        $window.ga('create', 'UA-88934895-1', 'auto');
+ 
+        // track pageview on state change
+        $rootScope.$on('$stateChangeSuccess', function (event) {
+            $window.ga('send', 'pageview', $location.path());
+        });
+    }
